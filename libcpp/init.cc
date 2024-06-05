20fc0470e23e3cfe0c25005114fed72443ff7735
Matthias Klose <doko@ubuntu.com>
2024-06-05 10:25:07 +0200

    gcc-textdomain.diff
    # DP: Set gettext's domain and textdomain to the versioned package name.

--- libcpp/init.cc
+++ libcpp/init.cc
@@ -210,7 +210,7 @@ init_library (void)
       init_trigraph_map ();
 
 #ifdef ENABLE_NLS
-       (void) bindtextdomain (PACKAGE, LOCALEDIR);
+       (void) bindtextdomain (PACKAGE PACKAGE_SUFFIX, LOCALEDIR);
 #endif
     }
 }
